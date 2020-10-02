86b11b1ef421d040ed16d4c0d0dd0b0aa1e741b6
maximus <sanjanadhikari1@gmail.com>
2020-10-03 02:54:21 +0800

    update

--- 11-use-context/src/demo.js
+++ 11-use-context/src/demo.js
@@ -1,26 +1,26 @@
 import React from "react";
 
 const MyContext = React.createContext({
-  username: "",
-  setUsername: () => {}
+    username: "",
+    setUsername: () => {}
 });
 
 export const MyContextProvider = props => {
-  const [username, setUsername] = React.useState("John Doe");
+    const [username, setUsername] = React.useState("John Doe");
 
-  return (
-    <MyContext.Provider value={{ username, setUsername }}>
-      {props.children}
-    </MyContext.Provider>
-  );
+    return (
+        <MyContext.Provider value={{ username, setUsername }}>
+            {props.children}
+        </MyContext.Provider>
+    );
 };
 
 export const MyComponent = () => {
-  const myContext = React.useContext(MyContext);
+    const myContext = React.useContext(MyContext);
 
-  return (
-    <>
-      <h3>{myContext.username}</h3>
-    </>
-  )
+    return (
+        <>
+            <h3>{myContext.username}</h3>
+        </>
+    )
 }
--- 11-use-context/src/index.js
+++ 11-use-context/src/index.js
@@ -4,13 +4,13 @@ import { MyComponent, MyContextProvider } from "./demo";
 import "./styles.css";
 
 function App() {
-  return (
-    <MyContextProvider>
-      <div className="App">
-        <MyComponent />
-      </div>
-    </MyContextProvider>
-  );
+    return (
+        <MyContextProvider>
+            <div className="App">
+                <MyComponent />
+            </div>
+        </MyContextProvider>
+    );
 }
 
 const rootElement = document.getElementById("root");
