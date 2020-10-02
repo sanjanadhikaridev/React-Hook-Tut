e8a535c48a013298bb93e2d5891277a814ee5853
sanjanadhikaridev <maximus0427@outlook.com>
2020-10-03 03:44:09 +0800

    update

--- 12-async-closure/src/demo.js
+++ 12-async-closure/src/demo.js
@@ -3,9 +3,7 @@ import React from "react";
 export const MyComponent = () => {
     const [message, setMessage] = React.useState("initial message");
     const [seconds, setSeconds] = React.useState(0);
-
     const secondsRef = React.useRef(seconds);
-
     React.useEffect(() => {
         setTimeout(() => {
             console.log(seconds);
@@ -15,7 +13,6 @@ export const MyComponent = () => {
 
         setTimeout(() => {
             setMessage(`Total seconds: ${secondsRef.current}`);
-            
         }, 2000);
     }, []);
 
--- 15-memo-predicate/src/demo.js
+++ 15-memo-predicate/src/demo.js
@@ -2,39 +2,39 @@ import * as React from 'react';
 
 const setSatisfactionClass = level => {
 
-  if (level < 100) {
-    return "very-dissatisfied"
-  }
+    if (level < 100) {
+        return "very-dissatisfied"
+    }
 
-  if (level < 200) {
-    return "somewhat-dissatisfied"
-  }
+    if (level < 200) {
+        return "somewhat-dissatisfied"
+    }
 
-  if (level < 300) {
-    return "neither"
-  }
+    if (level < 300) {
+        return "neither"
+    }
 
-  if (level < 400) {
-    return "somewhat-satisfied"
-  }
+    if (level < 400) {
+        return "somewhat-satisfied"
+    }
 
-  return "very-satisfied"
+    return "very-satisfied"
 }
 
 const isSameRange = (prevValue, nextValue) => {
 
-  const prevValueClass = setSatisfactionClass(prevValue.level);
-  const nextValueClass = setSatisfactionClass(nextValue.level);
+    const prevValueClass = setSatisfactionClass(prevValue.level);
+    const nextValueClass = setSatisfactionClass(nextValue.level);
 
-  return prevValueClass === nextValueClass;
+    return prevValueClass === nextValueClass;
 }
 
 export const FaceComponent = React.memo(props => {
 
-  const { level } = props;
+    const { level } = props;
 
-  return (
-    <div className={setSatisfactionClass(level)} />
-  );
+    return (
+        <div className={setSatisfactionClass(level)} />
+    );
 
 }, isSameRange);
